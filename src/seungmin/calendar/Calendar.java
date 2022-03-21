7797e52850a4c02094c6bb3bc5bd0fc4f09aec72
zmdals <77082787+zmdals@users.noreply.github.com>
2022-03-21 14:03:31 +0900

    Add leap year

--- src/seungmin/calendar/Calendar.java
+++ src/seungmin/calendar/Calendar.java
@@ -3,15 +3,25 @@ package seungmin.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	private static final int[] daysOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31}; 
-	public int getMaxDaysOfMonth(int month) {
-		return daysOfMonth[month-1];
+	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31}; 
+	private static final int[] LEAP_MAX_DAYS = {31,29,31,30,31,30,31,31,30,31,30,31}; 
+	public int getMaxDaysOfMonth(int year,int month) {
+		if(isLeapYear(year)){
+			return LEAP_MAX_DAYS[month-1];
+		}
+		return MAX_DAYS[month-1];
+	}
+	public boolean isLeapYear(int year) {
+		if(year%4 == 0 && (year % 100 != 0 || year % 400 == 0)) 
+			return true;
+		else
+			return false;
 	}
 	public void printCalendar(int year,int month) {
 		System.out.printf("    <<%4d년,%3d월>>\n", year,month);//각각 4칸,3칸을 차지한다는 뜻.
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println(" --------------------");
-		int maxDay = getMaxDaysOfMonth(month);
+		int maxDay = getMaxDaysOfMonth(year,month);
 		for(int i=1; i<=maxDay;i++) {
 			System.out.printf("%3d",i);
 			if(i%7==0) System.out.println();
@@ -23,20 +33,6 @@ public class Calendar {
 //		System.out.println("22 23 24 25 26 27 28");
 	}
 	public static void main(String[] args) {
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		Scanner sc = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		
-		while(true) {
-		System.out.println("달을 입력해주십시요.");
-		int a = sc.nextInt();
-		if(a < 1) break;
-		if(a > 12) continue;
-		int m = cal.getMaxDaysOfMonth(a);
-		
-		}
-		System.out.println("Bye!");
-		sc.close();
 
 	}
 
--- src/seungmin/calendar/Prompt.java
+++ src/seungmin/calendar/Prompt.java
@@ -9,15 +9,18 @@ public class Prompt {
 		Calendar cal = new Calendar();
 		
 		while(true) {
+		System.out.println("연도 입력해주십시요.");
+		System.out.println("YEAR>");
+		int year = sc.nextInt();
 		System.out.println("달을 입력해주십시요.");
-		System.out.print(PROMPT);
+		System.out.println("MONTH>");
 		int month = sc.nextInt();
 		if(month < 1) break;
 		if(month > 12) continue;
-		int m = cal.getMaxDaysOfMonth(month);
-		System.out.println(month+"월은 "  + m + "일까지 있습니다.");
+		int m = cal.getMaxDaysOfMonth(year,month);
+		System.out.println(year+"년도 " + month+"월은 "  + m + "일까지 있습니다.");
 //		System.out.printf("%d월은 %d일까지 있습니다.",a,m);
-		cal.printCalendar(2017, month);
+		cal.printCalendar(year, month);
 		}
 		System.out.println("Bye!");
 		sc.close();
